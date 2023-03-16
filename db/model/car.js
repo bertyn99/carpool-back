3b9d3a0540fa4eb288e8c4c4d9e1497f73409889
Bertyn Boulikou <bboulikou@gmail.com>
2023-03-16 11:26:37 +0100

    feat(model): add model car

---
+++ db/model/car.js
@@ -0,0 +1,30 @@
+import prisma from "../../prisma/prisma";
+
+class CarModel {
+  async getCarByCarOwner(carOwner) {
+    const car = await prisma.car.findUnique({
+      where: {
+        carOwner,
+      },
+    });
+    return car;
+  }
+  async getCarByCarModel(carModel) {
+    const car = await prisma.car.findUnique({
+      where: {
+        carModel,
+      },
+    });
+    return car;
+  }
+  async getCarByNbPlate(nbPlate) {
+    const car = await prisma.car.findUnique({
+      where: {
+        nbPlate,
+      },
+    });
+    return car;
+  }
+}
+
+export default CarModel;
