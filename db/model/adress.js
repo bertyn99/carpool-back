e9e97427ea1aac27b686f33169f4a5e7f78368b5
AlanAudran <65293707+AlanAudran@users.noreply.github.com>
2023-03-16 12:01:52 +0100

    feat(bdd):add model adress

--- db/model/adress.js
+++ db/model/adress.js
@@ -5,19 +5,56 @@ class addressModel{
         this.prisma = prisma;
     }
     async create(address){
-        const {user_id, address1, address2, city, state, country, zipcode} = address;
+        const {street, city, zipCode, long, lat} = address;
         const newAddress = await this.prisma.address.create({
             data:{
                 street,
                 city,
-                city,
                 zipCode,
                 long,
-                lat,
-                zipcode
+                lat
             }
         });
         return newAddress;
+    }
+    async update(id, address){
+        const {street, city, zipCode, long, lat} = address;
+        const updatedAddress = await this.prisma.address.update({
+            where:{
+                id
+            },
+            data:{
+                street,
+                city,
+                zipCode,
+                long,
+                lat
+        }});
+        return updatedAddress;
+    }
+    async delete(id){
+        const deletedAddress = await this.prisma.address.delete({
+            where:{
+                id
+            }
+        });
+        return deletedAddress;
+    }
+    async getAddressById(id){
+        const address = await this.prisma.address.findUnique({
+            where:{
+               id
+            }
+        });
+        return address;
+    }
+    async getAddressByTripId(trip_id){
+        const address = await this.prisma.address.findUnique({
+            where:{
+               trip_id
+            }
+        });
+        return address;
     }
 }
 
