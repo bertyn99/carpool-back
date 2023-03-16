07e354f2af7609c58ec9b15ad09f06515f5b2d8c
Bertyn Boulikou <bboulikou@gmail.com>
2023-03-16 11:49:07 +0100

    feat(message):  add message model and fix value default date

---
+++ db/model/message.js
@@ -0,0 +1,47 @@
+import prisma from "../../prisma/prisma";
+
+class Message {
+  constructor() {
+    this.prisma = prisma;
+  }
+  async create(userId, tripId, content) {
+    const newMessage = await prisma.message.create({
+      data: {
+        userId,
+        tripId,
+        content,
+      },
+    });
+    return newMessage;
+  }
+  async update(userId, tripId, content) {
+    const updatedMessage = await prisma.message.update({
+      where: {
+        userId,
+        tripId,
+      },
+      data: {
+        content,
+      },
+    });
+    return updatedMessage;
+  }
+  async delete(tripId, messageId) {
+    const deletedMessage = await prisma.message.delete({
+      where: {
+        tripId,
+        messageId,
+      },
+    });
+  }
+  async getMessagesOfaTrip(tripId) {
+    const messages = await prisma.message.findMany({
+      where: {
+        tripId,
+      },
+    });
+    return messages;
+  }
+}
+
+export default Message;
