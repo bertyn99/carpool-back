8633f4fe7d3dca3415879c524b69f2ddd0d0d04b
bertyn99 <bboulikou@gmail.com>
2023-03-23 16:05:24 +0100

    refacto(file): move route to route directory

---
+++ routes/auth.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { register, logIn } from "../controllers/user.js";
+import verifyToken from "../services/verifyToken.js";
+
+export const router = (function () {
+  let apiRouter = Router();
+
+  //healthCheck
+  apiRouter.get("/", (req, res) => {
+    res.send("API is running");
+  });
+  // register user
+  apiRouter.route("/register").post(register);
+
+  // connection user
+  apiRouter.route("/login").post(logIn);
+
+  return apiRouter;
+})();
