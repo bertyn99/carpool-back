956567f2ea20ec4d34d5cdcc090587e6fa65d307
Bertyn Boulikou <bboulikou@gmail.com>
2023-04-06 11:51:39 +0200

    feat(user): wip register route

--- controllers/user.js
+++ controllers/user.js
@@ -3,12 +3,13 @@
 import User from "../db/model/user.js";
 import { successRes, errorRes } from "../common/response.js";
 import UserService from "../services/userServices.js";
+
 async function register(req, res) {
   try {
-    const { name, email, password, phone } = req.body;
+    const { name, email, password, tel } = req.body;
 
-    const user = await UserService.createUser({ name, email, password, phone });
-    successRes(res, "register");
+    const user = await UserService.createUser({ name, email, password, tel });
+    successRes(res, user, 202);
   } catch (e) {
     errorRes(res, e, 400);
   }
