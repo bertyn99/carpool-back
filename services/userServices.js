4528d083ae4929da13f495c777196c9b64bbdaf8
bertyn99 <bboulikou@gmail.com>
2023-04-09 21:15:26 +0200

    chore: add doc in the service

--- services/userServices.js
+++ services/userServices.js
@@ -4,24 +4,41 @@ import { createSigner, createVerifier } from "fast-jwt";
 import argon2 from "argon2";
 const db = UserModel;
 //signer
-const signerToken = createSigner({ expiresIn: 1000 * 60 * 10, key: "secret" });
+const signerToken = createSigner({ expiresIn: 1000 * 60 * 10, key: "secret" });//10 minutes
 const signerRefreshToken = createSigner({
-  expiresIn: 1000 * 60 * 60 * 24 * 7,
+  expiresIn: 1000 * 60 * 60 * 24 * 7,//7 days
   key: "secret",
 });
 // Standard decoder
 const decode = createVerifier({ key: "secret" });
+
+
 const UserService = {
+  /**
+   * name: getUsers
+   * @description: get all users
+   * @returns {Promise<User[]>}
+   */
   async getUsers() {
     const users = await db.getUsers();
     return users;
   },
 
+  /**
+   * @description: get user by id
+   * @param {} id 
+   * @returns {Promise<User>}
+   */
   async getUser(id) {
     const user = await db.getUserById(id);
     return user;
   },
 
+  /**
+   * @description: login user
+   * @param {*} data 
+   * @returns 
+   */
   async loginUser(data) {
     const user = await db.getUserByEmail(data.email);
     if (!user) {
@@ -65,6 +82,12 @@ const UserService = {
     return { ...user, ...token };
   },
 
+  /**
+   * 
+   * @param {*} id 
+   * @param {*} data 
+   * @returns 
+   */
   async updateUser(id, data) {
     const user = await db.update(id, data);
     return user;
@@ -87,11 +110,39 @@ const UserService = {
     return user;
   },
 
+  /**
+   * @description: generate both access_token and refresh_token
+   * @param {*} payload 
+   * @returns 
+   */
   async generateToken(payload) {
-    const access = signerToken({ ...payload });
-    const refresh = signerRefreshToken({ ...payload });
+    const access =await generateAccessToken({ ...payload});
+    const refresh =await  generateRefreshToken({ ...payload });
     return { access_token: access, refresh_token: refresh };
   },
+
+  /**
+   * @description: generate access_token
+   * @param {*} payload 
+   * @returns 
+   */
+  async generateAccessToken(payload){
+    return signerToken({ ...payload });;
+  },
+    /**
+   * @description: generate refresh_token
+   * @param {*} payload 
+   * @returns 
+   */
+  async generateRefreshToken(){
+    return signerRefreshToken({ ...payload });;
+  },
+
+  /**
+   * 
+   * @param {*} token 
+   * @returns 
+   */
   async decodeToken(token) {
     const payload = await decode(token);
     return payload;
